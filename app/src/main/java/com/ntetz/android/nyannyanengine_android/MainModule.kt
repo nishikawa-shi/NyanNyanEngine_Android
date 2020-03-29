576239af01da6c353423406c54310139787889d5
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2020-03-29 16:08:11 +0900

    エントリーポイントの画面用のFragmentを追加

--- app/src/main/java/com/ntetz/android/nyannyanengine_android/MainModule.kt
+++ app/src/main/java/com/ntetz/android/nyannyanengine_android/MainModule.kt
@@ -1,8 +1,11 @@
 package com.ntetz.android.nyannyanengine_android
 
+import com.ntetz.android.nyannyanengine_android.ui.main.MainViewModel
+import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 private val viewModelModule = module {
+    viewModel { MainViewModel() }
 }
 
 object MainModule {
---
+++ app/src/main/java/com/ntetz/android/nyannyanengine_android/ui/main/MainViewModel.kt
@@ -0,0 +1,5 @@
+package com.ntetz.android.nyannyanengine_android.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel()
