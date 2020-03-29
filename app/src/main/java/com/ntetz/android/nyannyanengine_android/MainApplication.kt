546dd13867570d65c06e9c768a5a5de97315f4e5
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2020-03-29 16:05:02 +0900

    ViewModel注入用のトップレベルのクラスを追加

---
+++ app/src/main/java/com/ntetz/android/nyannyanengine_android/MainApplication.kt
@@ -0,0 +1,15 @@
+package com.ntetz.android.nyannyanengine_android
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@MainApplication)
+            modules(MainModule.modules)
+        }
+    }
+}
---
+++ app/src/main/java/com/ntetz/android/nyannyanengine_android/MainModule.kt
@@ -0,0 +1,10 @@
+package com.ntetz.android.nyannyanengine_android
+
+import org.koin.dsl.module
+
+private val viewModelModule = module {
+}
+
+object MainModule {
+    val modules = listOf(viewModelModule)
+}
