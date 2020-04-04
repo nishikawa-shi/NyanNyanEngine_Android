0931e36a225f5505597f0daf852d444dacd65c1d
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2020-04-04 21:23:30 +0900

    Drawerメニューの項目をタップした時、メニューが閉じるようにした

--- app/src/main/java/com/ntetz/android/nyannyanengine_android/MainActivity.kt
+++ app/src/main/java/com/ntetz/android/nyannyanengine_android/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.ntetz.android.nyannyanengine_android
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
@@ -26,9 +29,18 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph, main_drawer)
         setupActionBarWithNavController(navController, appBarConfiguration)
         main_nav_view.setupWithNavController(navController)
+        main_nav_view.setNavigationItemSelectedListener(this)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return findNavController(R.id.main_nav_fragment).navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        main_drawer.closeDrawers()
+        return NavigationUI.onNavDestinationSelected(
+            item,
+            findNavController(R.id.main_nav_fragment)
+        ) || super.onOptionsItemSelected(item)
     }
 }
