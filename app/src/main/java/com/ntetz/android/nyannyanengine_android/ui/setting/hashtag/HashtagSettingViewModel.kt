fbee40600986aa60c2ac67431b8810024718f2c3
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2020-04-04 21:01:31 +0900

    はっしゅたぐ設定画面の基盤を追加

---
+++ app/src/main/java/com/ntetz/android/nyannyanengine_android/ui/setting/hashtag/HashtagSettingViewModel.kt
@@ -0,0 +1,7 @@
+package com.ntetz.android.nyannyanengine_android.ui.setting.hashtag
+
+import androidx.lifecycle.ViewModel
+
+class HashtagSettingViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
