4a30ff042a8c88ea69b0883700cc22b20230e1ef
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2020-08-02 23:24:07 +0900

    apk化した後デコンパイルしても、秘匿系の情報が見えづらくなるようにした
    当該のファイルはGitで追跡するのではなく、
    azure-pipelineのlibraryとして管理しCI実行時に注入する。

--- app/src/dev/java/com/ntetz/android/nyannyanengine_android/model/config/TwitterConfig.kt
+++ app/src/dev/java/com/ntetz/android/nyannyanengine_android/model/config/TwitterConfig.kt
@@ -1,7 +1,18 @@
 package com.ntetz.android.nyannyanengine_android.model.config
 
 class TwitterConfig : ITwitterConfig {
-    override val callbackUrl: String = "https://nyannyanengine-ios-d.firebaseapp.com/authorized/"
-    override val apiSecret: String = "jkl456MNO"
-    override val consumerKey: String = "abcde123GHI"
+    override val callbackUrl: String = "https://nyannyanengine-android-d.firebaseapp.com/authorized/"
+    override val apiSecret: String
+        get() = getApiSecretFromJniDev()
+    override val consumerKey: String
+        get() = getConsumerKeyFromJniDev()
+
+    private external fun getApiSecretFromJniDev(): String
+    private external fun getConsumerKeyFromJniDev(): String
+
+    companion object {
+        init {
+            System.loadLibrary("secure-config-master-lib")
+        }
+    }
 }
