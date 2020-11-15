5dce6dced9c9d0b7b3980f379e2bbfefb50d0763
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2020-11-15 09:26:15 +0900

    開発系APIのリダイレクト先をiOSのものに合わせた
    名前に反して両OS共通の設定なのだが、
    Android専用のドメイン用とかを用意するのが面倒くさくなった

--- app/src/dev/java/com/ntetz/android/nyannyanengine_android/model/config/TwitterConfig.kt
+++ app/src/dev/java/com/ntetz/android/nyannyanengine_android/model/config/TwitterConfig.kt
@@ -1,7 +1,7 @@
 package com.ntetz.android.nyannyanengine_android.model.config
 
 class TwitterConfig : ITwitterConfig {
-    override val callbackUrl: String = "https://nyannyanengine-android-d.firebaseapp.com/authorized/"
+    override val callbackUrl: String = "https://nyannyanengine-ios-d.firebaseapp.com/authorized/"
     override val apiSecret: String
         get() = getApiSecretFromJniDev()
     override val consumerKey: String
