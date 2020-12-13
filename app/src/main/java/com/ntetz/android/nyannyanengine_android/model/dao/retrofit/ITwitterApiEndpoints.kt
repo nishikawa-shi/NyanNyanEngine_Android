e6f0bd910271e7b3e8b0e251a84da0c53ba2d38a
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2020-12-13 11:04:06 +0900

    ホームタイムラインAPIのエンドポイント情報を追加

--- app/src/main/java/com/ntetz/android/nyannyanengine_android/model/dao/retrofit/ITwitterApiEndpoints.kt
+++ app/src/main/java/com/ntetz/android/nyannyanengine_android/model/dao/retrofit/ITwitterApiEndpoints.kt
@@ -2,6 +2,7 @@ package com.ntetz.android.nyannyanengine_android.model.dao.retrofit
 
 import com.ntetz.android.nyannyanengine_android.model.config.TwitterEndpoints
 import retrofit2.Call
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Query
@@ -16,4 +17,7 @@ interface ITwitterApiEndpoints {
         @Query("oauth_token") oauthToken: String,
         @Header(TwitterEndpoints.authorizationHeaderName) authorization: String
     ): Call<String>
+
+    @GET(TwitterEndpoints.homeTimelinePath)
+    fun getTweets(@Header(TwitterEndpoints.authorizationHeaderName) authorization: String): Call<String>
 }
