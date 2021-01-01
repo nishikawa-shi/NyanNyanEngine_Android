8c0cdb55c7f9c2059fa48418ed6b27aa27499363
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2021-01-01 09:46:40 +0900

    ユーザーの行動に応じて指標を飛ばす機構を追加

--- app/src/main/java/com/ntetz/android/nyannyanengine_android/model/entity/dao/firebase/AnalyticsEvent.kt
+++ app/src/main/java/com/ntetz/android/nyannyanengine_android/model/entity/dao/firebase/AnalyticsEvent.kt
@@ -1,3 +1,17 @@
 package com.ntetz.android.nyannyanengine_android.model.entity.dao.firebase
 
-enum class AnalyticsEvent(val title: String)
+enum class AnalyticsEvent(val title: String) {
+    TAP_POST_NEKOGO("tap__post_nekogo"),
+    COMPLETE_POST_NEKOGO("complete__post_nekogo"),
+
+    TAP_SIGN_IN("tap__sign_in"),
+    COMPLETE_SIGN_IN("complete__sign_in"),
+
+    TAP_SIGN_OUT("tap__sign_out"),
+    COMPLETE_SIGN_OUT("complete__sign_out"),
+
+    TAP_SETTING_HASHTAG("tap__setting_hashtag"),
+    COMPLETE_SETTING_HASHTAG("complete__setting_hashtag"),
+
+    TAP_TWEET("tap__tweet"),
+}
