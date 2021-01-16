0cb1cfa0aebbf17a2a52f6076ed581a6362ce437
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2021-01-16 14:44:43 +0900

    IHashtagsRepositoryに対応するインスタンスをHiltから受け取れるようにした

--- app/src/main/java/com/ntetz/android/nyannyanengine_android/ApplicationBindsModule.kt
+++ app/src/main/java/com/ntetz/android/nyannyanengine_android/ApplicationBindsModule.kt
@@ -1,7 +1,11 @@
 package com.ntetz.android.nyannyanengine_android
 
+import com.ntetz.android.nyannyanengine_android.model.config.DefaultHashtagConfig
+import com.ntetz.android.nyannyanengine_android.model.config.IDefaultHashtagConfig
 import com.ntetz.android.nyannyanengine_android.model.dao.firebase.FirebaseClient
 import com.ntetz.android.nyannyanengine_android.model.dao.firebase.IFirebaseClient
+import com.ntetz.android.nyannyanengine_android.model.repository.HashtagsRepository
+import com.ntetz.android.nyannyanengine_android.model.repository.IHashtagsRepository
 import com.ntetz.android.nyannyanengine_android.model.repository.IMetricsRepository
 import com.ntetz.android.nyannyanengine_android.model.repository.MetricsRepository
 import dagger.Binds
@@ -14,6 +18,12 @@ import dagger.hilt.components.SingletonComponent
 abstract class ApplicationBindsModule {
     @Binds
     abstract fun bindFirebaseClient(firebaseClient: FirebaseClient): IFirebaseClient
+
+    @Binds
+    abstract fun bindDefaultHashtagConfig(defaultHashtagConfig: DefaultHashtagConfig): IDefaultHashtagConfig
+
+    @Binds
+    abstract fun bindHashtagsRepository(hashtagsRepository: HashtagsRepository): IHashtagsRepository
 
     @Binds
     abstract fun bindMetricsRepository(metricsRepository: MetricsRepository): IMetricsRepository
